bf4131f7beb05dda9286200da6ad9e0564654275
Rakibul Hasan <rakibulhasan745101@gmail.com>
2026-01-21 23:51:40 +0600

    13th chapter is on the way to end

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,35 +10,57 @@ const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, 'Please enter an amount greater than $0.'),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string(),
 });
 
 const CreateInvoiceSchema = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoiceSchema.parse(Object.fromEntries(formData.entries()))
-  // Test it out:
-  console.log({ customerId, amount, status });
+export type State = {
+    errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
 
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoiceSchema.safeParse(Object.fromEntries(formData.entries()))
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
+  const date = new Date().toISOString().split('T')[0];
 
+   // Insert data into the database
   try {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return {
+    // If a database error occurs, return a more specific error.
+    console.error(error, {
       message: 'Database Error: Failed to Create Invoice.',
-    };
+    });
   }
 
+    // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -60,8 +82,7 @@ try {
   `;
 } catch (error) {
     // We'll also log the error to the console for now
-    console.error(error);
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    console.error(error, { message: 'Database Error: Failed to Update Invoice.' });
 }
 
 
