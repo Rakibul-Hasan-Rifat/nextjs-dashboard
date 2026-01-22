c02a2263242270bc136609112092d80f55893105
Rakibul Hasan <rakibulhasan745101@gmail.com>
2026-01-22 20:11:10 +0600

    13th chapter is completely accomplished

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,11 +11,11 @@ const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
+    message: 'Please select a customer.',
   }),
   amount: z.coerce.number().gt(0, 'Please enter an amount greater than $0.'),
   status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Please select an invoice status.'
+    message: 'Please select an invoice status.'
   }),
   date: z.string(),
 });
